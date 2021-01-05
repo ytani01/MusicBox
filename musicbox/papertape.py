8211c8ac7cfb73534f4eebb2a37789ce41a61c03
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2021-01-06 04:14:38 +0900

    [add] musicbox/papertape.py

--- musicbox/papertape.py
+++ musicbox/papertape.py
@@ -18,6 +18,11 @@ class PaperTape(Parser):
     """
     PaperTape parser for Music Box
     """
+    COMMENT_CHR = '#'
+
+    ON_CHR = 'oO*'
+    OFF_CHR = '-_'
+
     def __init__(self, debug=False):
         """ Constructor """
         self._dbg = debug
@@ -34,3 +39,60 @@ class PaperTape(Parser):
         music_data: list of MusicDataEnt
         """
         self._log.debug('infile=%s', infile)
+
+        with open(infile) as f:
+            lines = f.readlines()
+
+        music_data = []
+        delay_unit_msec = 0
+        delay_msec = 0
+        abs_time_sec = 0
+        for line in lines:
+
+            # remove comment
+            comment_i = None
+            try:
+                comment_i = line.index(self.COMMENT_CHR)
+            except ValueError as ex:
+                pass  # ignore
+
+            if comment_i is not None:
+                line = line[0:comment_i]
+
+            word = line.split()
+
+            if len(word) == 0:
+                # comment or empty line
+                continue
+
+            try:
+                delay_unit_msec = int(word[0])
+                self._log.debug('delay_unit_msec=%s', delay_unit_msec)
+                continue
+            except ValueError:
+                pass
+
+            ch = []
+            for i, c in enumerate(list(word[0])):
+                if c in self.ON_CHR:
+                    ch.append(i)
+
+            if ch:
+                ent = {'abs_time': round(abs_time_sec, 3),
+                       'delay': round(delay_msec, 1),
+                       'ch': ch}
+                music_data.append(ent)
+                print(ent)
+
+                delay_msec = 0
+
+            delay_msec += delay_unit_msec
+            abs_time_sec += delay_unit_msec / 1000
+
+        ent = {'abs_time': round(abs_time_sec, 3),
+               'delay': round(delay_msec, 1),
+               'ch': []}
+        music_data.append(ent)
+        print(ent)
+
+        return music_data
