3f510e524dbf4d72a1900f06443262adc46e0ea9
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2021-01-07 06:32:39 +0900

    update

--- boot-musicbox.sh
+++ boot-musicbox.sh
@@ -20,4 +20,4 @@ ${MUSICBOX_CMD} svr -w 1 -p 8881 >> $LOGDIR/svr1.log 2>&1 &
 ${MUSICBOX_CMD} svr -w 2 -p 8882 >> $LOGDIR/svr2.log 2>&1 &
 ${MUSICBOX_CMD} svr -w 3 -p 8883 >> $LOGDIR/svr3.log 2>&1 &
 ${MUSICBOX_CMD} calibration >> $LOGDIR/calibraion.log 2>&1 &
-sleep 2
+sleep 3
